53a91ee2202d44077d08ffb05123851794fc7a92
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-08-23 11:20:50 -0700

    refactor: Run format

--- ava-rule-tester.js
+++ ava-rule-tester.js
@@ -1,19 +1,25 @@
 const RuleTester = require('eslint').RuleTester;
 
-module.exports = function (test, options) {
-  RuleTester.describe = function (text, method) {
+module.exports = function(test, options) {
+  RuleTester.describe = function(text, method) {
     RuleTester.it.validity = text;
     return method.apply(this);
   };
 
-  RuleTester.it = function (text, method) {
-    test(RuleTester.it.validity + ': ' + text, function (t) {
+  RuleTester.it = function(text, method) {
+    test(RuleTester.it.validity + ': ' + text, function(t) {
       t.pass();
       try {
         method();
       } catch (err) {
         if (err.message.indexOf('Output is incorrect') !== -1) {
-          err.message += `\n\nActual:\n${err.actual}\n\nExpected:\n${err.expected}`;
+          err.message += `
+Actual:
+${err.actual}
+
+Expected:
+
+${err.expected}`;
         }
         throw err;
       }
